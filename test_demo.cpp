443a985f167eecae38381fccf9e1686150792e23
Canhui Zhang <2606255465@qq.com>
2022-01-05 22:27:02 +0800

    20220105 | Struggling for the O(n) algorithm

--- test_demo.cpp
+++ test_demo.cpp
@@ -5,14 +5,21 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <deque>
+#include <queue>
+#include <stack>
 
 using std::cout;
 using std::endl;
 using std::max;
 using std::min;
+using std::greater;
 using std::pair;
 using std::string;
 using std::vector;
+using std::deque;
+using std::priority_queue;
+using std::stack;
 
 struct TreeNode
 {
@@ -60,7 +67,7 @@ struct Path
         }
     }
 
-    vector<int> CaluF(const vector<TreeNode> &path, int W)
+    vector<int> CaluFOn2(const vector<TreeNode> &path, int W)
     {
 
         if (path.size() == 0 || path.size() == 1)
@@ -96,6 +103,54 @@ struct Path
         }
 
         return F;
+    }
+
+    vector<int> CaluFOn(const vector<TreeNode> &path, int W)
+    {
+
+        if (path.size() == 0 || path.size() == 1)
+        {
+            throw "Empty, please cheak your path";
+        }
+
+        for(int ii=1;ii<path.size();ii++){ //TODO preprecessing to confirm the situation of each "j"
+            
+        }
+
+        // initialize F array
+        vector<int> F(path.size(), 0);    // 1 -> n
+        F[1] = IntervalFuncS(path, 1, 1); // index: 1
+        priority_queue<int,vector<int> ,greater<int> > cost;
+        deque<int> J;
+        J.push_front(0);
+        for(int ii=2; ii < path.size(); ii++){ // F[1]->F[2]->F[3]->...->F[n]
+            if(!J.empty()){
+                while(!J.empty()&&IntervalSumW(path,J.front()+1,ii)>W){
+                    J.pop_front();
+                }
+                while(!J.empty()&&path[J.back()]._s<=path[ii]._s){
+                    J.pop_back();
+                }
+            }
+            if(path[ii-1]._s>path[ii]._s){ // accord with the definition of s-maximal option
+                J.push_back(ii-1);
+                cost.push(F[ii-1]+path[ii]._s);
+            }
+
+        }
+
+    }
+
+    void Renew(){
+
+    }
+
+    void CaluMin(){
+
+    }
+
+    int SMaximalOption(const vector<TreeNode>& path,int j,int i){
+
     }
 
     int IntervalSumW(const vector<TreeNode> &path, int startInd, int endInd)
@@ -170,7 +225,7 @@ struct TestModule
         {
             Path path(n, w, s);
             path.PrintPath();
-            vector<int> F = path.CaluF(path._testPath, W);
+            vector<int> F = path.CaluFOn2(path._testPath, W);
             for (int ii = 1; ii < F.size(); ii++)
             {
                 cout << "F[" << ii << "]:" << F[ii] << " ";
@@ -186,6 +241,30 @@ struct TestModule
         }
     }
 };
+
+vector<int> AllNearestSmallerValues_SequentialAlg(const vector<int>& seq){
+    if(seq.size()==0){
+        throw "Empty vector";
+    }
+
+    vector<int> preVal(seq.size(),0);
+    stack<int> S; // use stack to maintain the "nearest" value
+    for(int ii=0;ii<seq.size();ii++){
+        while(!S.empty()&&S.top()>=seq[ii]){ // last-in, first-out
+            S.pop();
+        }
+
+        if(S.empty()){
+            preVal[ii]=-1;
+        }else{
+            preVal[ii]=S.top();
+        }
+
+        S.push(seq[ii]);
+    }
+
+    return preVal;
+}
 
 int main()
 {
@@ -205,6 +284,12 @@ int main()
     TestModule T;
     T.testFunc(W1, n1, w1, s1);
     T.testFunc(W2, n2, w2, s2);
+
+    vector<int> seq={0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+    vector<int> res=AllNearestSmallerValues_SequentialAlg(seq);
+    for(auto e:res){
+        cout<<e<<" ";
+    }cout<<endl;
 
     return 0;
 }
